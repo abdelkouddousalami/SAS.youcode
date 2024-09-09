aec65a5f1551dd02e35daf032affdbd0ecc6caa4
abdoelkouddous.elalami <abdoalami.ru7@gmail.com>
2024-09-09 21:35:59 +0100

    repair

--- DAY1/variables/challenge6.c
+++ DAY1/variables/challenge6.c
@@ -1,29 +1,21 @@
 #include <stdio.h>
 int main() {
-    char opera;
     float a,b;
      printf("veuillez entrer premier nombre : ");
      scanf("%f",&a);
      printf("veuillez entrer deuxieme nombre : ");
      scanf("%f",&b);
-     printf("veuillez entrer  operation : '+' '-' '/' '*' :  ");
-     scanf(" %c",&opera);
      printf("\n");
         printf("\t\t\t resultat :\n");
         printf("\n");
-        switch(opera){
-             case '+' : printf("%.2f + %.2f = %.2f",a,b,a+b);
-             break;
-             case '-' : printf("%.2f - %.2f = %.2f",a,b,a-b);
-             break;
-             case '*' : printf("%.2f x %.2f = %.2f",a,b,a*b);
-             break;
-             case '/' : if (b == 0){
-                printf("nous ne pouvons pas diviser par 0");
-             }else{
-                printf("%.2f / %.2f = %.2f",a,b,a/b);
-            }
-            break;
+        printf("addition        : %.2f + %.2f  = %.2f\n",a,b,a+b);
+        printf("multiplication  : %.2f x %.2f  = %.2f\n",a,b,a*b);
+        printf("soustraction    : %.2f - %.2f  = %.2f\n",a,b,a-b);
+        if (b != 0){
+            printf("devision        : %.2f / %.2f  = %.2f\n",a,b,a/b);
+        }else{
+            printf("devision        :Error on ne peut pas diviser par 0");
+
         }
         printf("\n");
 return 0;
