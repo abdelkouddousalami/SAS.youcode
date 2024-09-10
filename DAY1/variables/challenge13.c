530db1f243e186829d634c814afd3e32ae71358e
abdelkouddous <abdoalalmi.ru@gmail.com>
2024-09-10 11:32:52 +0200

    repair

--- DAY1/variables/challenge13.c
+++ DAY1/variables/challenge13.c
@@ -2,16 +2,24 @@
 
 int main() {
     unsigned int numb;
+    int a=1;
        printf("entrez un nombre entier: ");
        scanf("%u", &numb);
        printf("\n");
           printf("hexadecimal est : %X\n", numb);
           printf("binary est : ");
-    for (int i = 31; i >= 0; i--) {
-          printf("%d", (numb >> i) & 1);
+            for (int i = 31; i >= 0; i--) {
+                if ((numb >> i) & 1) {
+                    a = 0;
+        }
+                if (!a) {
+                    printf("%d", (numb >> i) & 1);
+        }
     }
+        if (a) {
+            printf("0");
+        }
 
-    printf("\n");
-
+        printf("\n");
     return 0;
 }
