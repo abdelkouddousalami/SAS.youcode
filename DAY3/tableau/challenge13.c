6c9cff595e201bd80ed72b4c9deee79ec966d1d8
abdelkouddous <abdoalalmi.ru@gmail.com>
2024-09-12 17:44:56 +0200

    impair

---
+++ DAY3/tableau/challenge13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("veuillez entre votre nomber : ");
+    scanf("%d",&n);
+    int T[n];
+    for(i=0; i<n; i++){
+        printf("T[%d] : ",i+1);
+        scanf("%d",&T[i]);
+    }
+    printf("le elements pairs de tableau est : ");
+    for (i = 0; i < n; i++) {
+        if (T[i] % 2 != 0) {
+            printf("%d ",T[i]);
+        }
+    }
+
+    return 0;
+}
